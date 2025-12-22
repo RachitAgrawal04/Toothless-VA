2092be679bc648c396a7711b706617f941adac2f
RachitAgrawal04 <agrawalanush4@gmail.com>
2025-12-22 20:38:33 +0530

    Fixed some syntax errors

--- engine/features.py
+++ engine/features.py
@@ -2,5 +2,5 @@ from playsound import playsound
 
 # Function to play assistant sound
 def play_assistant_sound():
-    music_dir = 'www\assets\audio\start_sound.mp3'
+    music_dir = 'www/assets/audio/start_sound.mp3'
     playsound(music_dir)
