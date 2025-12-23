b746a6bf538f57098228d4a0d49ddfa22d768369
RachitAgrawal04 <agrawalanush4@gmail.com>
2025-12-23 08:03:37 +0530

    Add speech recognition and command functionality; update .gitignore and HTML structure

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,6 @@
 import pyttsx3
+import speech_recognition as sr
+import eel
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -8,4 +10,25 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-speak("Hello, I am your assistant.")
+#speak("This was supposed to be Hiccup's voice, but i suppose this will do for now.")
+
+@eel.expose
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source, 10, 7)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+        speak(query)
+
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    return query.lower()
+
