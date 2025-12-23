048d30dc5a22fb2ea41191fa004eb531caf6e150
RachitAgrawal04 <agrawalanush4@gmail.com>
2025-12-23 07:30:30 +0530

    Implement text-to-speech functionality in command.py

---
+++ engine/command.py
@@ -0,0 +1,11 @@
+import pyttsx3
+
+def speak(text):
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('rate', 178)
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Hello, I am your assistant.")
